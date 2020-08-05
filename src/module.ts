2c2bb32fbef3cc6fc5bf77c0460008878c42d3f3
Kreedzt <zhaozisong1@live.com>
2020-08-05 14:43:02 +0800

    :tada: Kreedzt - initial commit

---
+++ src/module.ts
@@ -0,0 +1,3 @@
+export const fnModule = (count: number) => {
+  console.log('count', count);
+}
---
+++ types/type.d.ts
@@ -0,0 +1,9 @@
+interface CT {
+  a: string;
+}
+
+type ES = {
+  es: string
+};
+
+type fn = (a: string) => string;
